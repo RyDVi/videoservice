1e5139d8093812219fee4c7b4599660b8dcf65b8
RyDVi <dmitrii.kardan.rch@gmail.com>
2023-05-02 22:43:58 +0300

    Refactoring

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,14 @@
 module.exports = {
   root: true,
   // This tells ESLint to load the config from the package `eslint-config-custom`
-  extends: ["custom"],
+  extends: [
+    "custom",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "prettier",
+  ],
   settings: {
     next: {
       rootDir: ["apps/*/"],
